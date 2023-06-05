a7a34ca94cd32d0769f56b203332d490757ea303
Dzmitry Liavonenka <dzmitryliavonenka@gmail.com>
2023-06-05 15:34:56 +0300

    feat: -added starts components and basic styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,37 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { ProfileComponent } from './home/profile/profile.component';
+import { MessagesComponent } from './home/messages/messages.component';
+import { NewsComponent } from './home/news/news.component';
+import { MusicComponent } from './home/music/music.component';
+import { MyPostsComponent } from './home/profile/my-posts/my-posts.component';
+import { PostComponent } from './home/profile/my-posts/post/post.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProfileComponent,
+    MessagesComponent,
+    NewsComponent,
+    MusicComponent,
+    MyPostsComponent,
+    PostComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatSlideToggleModule,
+    MatIconModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
